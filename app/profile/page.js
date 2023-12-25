125c6347f581c6521e99650a04585e030a10012d
Renu Rao <renurao.1306@gmail.com>
2023-12-25 16:03:28 +0530

    Added validation to profile page

--- app/profile/page.js
+++ app/profile/page.js
@@ -7,6 +7,50 @@ import {useState } from 'react'
 export default function profile(){
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [selectedOption, setSelectedOption] = React.useState(null);
+    const [name, setName] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [confirmPassword, setConfirmPassword] = React.useState("");
+    const [errors, setErrors] = React.useState({
+        name: "",
+        password: "",
+        confirmPassword: "",
+        general: ""
+    });
+
+    const handleClick = () => {
+        console.log("In handle click");                             //DEBUGGING
+        const errorMessage = {name: "", password: "", confirmPassword: "", general: ""};
+
+        if(selectedOption === null){
+            errorMessage.general = "Please select an option";
+        }
+        if(selectedOption === 'name' && !name.trim()){
+            errorMessage.name = "Name cannot be empty";
+        }
+        if(selectedOption === 'password'){
+            if(!password.trim()){
+                errorMessage.password = "Password cannot be empty";
+            }
+            if(!confirmPassword.trim()){
+                errorMessage.confirmPassword = "Please confirm your password"
+            }
+            if(password !== confirmPassword){
+                errorMessage.confirmPassword = "Passwords do not match"
+            }
+        }
+
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            ...errorMessage
+        }));
+
+
+        if(!Object.values(errorMessage).some((error) => error !== "")){
+            console.log("name: ", name);                            //TODO: replace with axios call
+            console.log("password: ", password);                    //TODO: replace with axios call
+            console.log("confirm password: ", confirmPassword);     //TODO: replace with axios call
+        }
+    }
 
     return(
         <div>
@@ -21,7 +65,16 @@ export default function profile(){
                                 className="form-radio text-indigo-600"
                                 value="name"
                                 checked={selectedOption === 'name'}
-                                onChange={() => setSelectedOption('name')}
+                                onChange={() => {
+                                    setSelectedOption('name');
+                                    setPassword("");
+                                    setConfirmPassword("");
+                                    setErrors((prevErrors) => ({
+                                        ...prevErrors,
+                                        password: "",
+                                        confirmPassword: ""
+                                    }));
+                                }}
                             />
                             <span className="ml-2">Update Name</span>
                         </label>
@@ -33,7 +86,16 @@ export default function profile(){
                                 type="text"
                                 className="border p-2"
                                 placeholder="Enter your new name"
+                                value={name}
+                                onChange={(event) => {
+                                    setName(event.target.value);
+                                    setErrors((prevErrors) => ({
+                                        ...prevErrors,
+                                        name: ""
+                                    }));
+                                }}
                             />
+                            <span className="text-red-500 ml-4">{errors.name}</span>
                         </div>
                     )}
 
@@ -44,7 +106,14 @@ export default function profile(){
                                 className="form-radio text-indigo-600"
                                 value="password"
                                 checked={selectedOption === 'password'}
-                                onChange={() => setSelectedOption('password')}
+                                onChange={() => {
+                                    setSelectedOption('password');
+                                    setName("");
+                                    setErrors((prevErrors) => ({
+                                        ...prevErrors,
+                                        name: ""
+                                    }));
+                                }}
                             />
                             <span className="ml-2">Update Password</span>
                         </label>
@@ -56,18 +125,36 @@ export default function profile(){
                                 type="password"
                                 className="border p-2 mb-2"
                                 placeholder="Enter your new password"
+                                value={password}
+                                onChange={(event) => {
+                                    setPassword(event.target.value);
+                                    setErrors((prevErrors) => ({
+                                        ...prevErrors,
+                                        password: ""
+                                    }));
+                                }}
                             />
-                            <br/>
+                            <span className="text-red-500 ml-4">{errors.password}</span>
+                            <br />
                             <input
                                 type="password"
                                 className="border p-2"
                                 placeholder="Confirm your new password"
+                                value={confirmPassword}
+                                onChange={(event) => {
+                                    setConfirmPassword(event.target.value);
+                                    setErrors((prevErrors) => ({
+                                        ...prevErrors,
+                                        confirmPassword: ""
+                                    }));
+                                }}
                             />
+                            <span className="text-red-500 ml-4">{errors.confirmPassword}</span>
                         </div>
                     )}
 
                     <div className="gap-x-6 mr-60">
-                        <button type="submit" className="rounded-md bg-indigo-600 mt-8 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        <button type="submit" className="rounded-md bg-indigo-600 mt-8 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={handleClick}>
                             Submit
                         </button>
                         <button type="button" className="text-sm font-semibold ml-10 mt-5 leading-6 text-gray-900">
