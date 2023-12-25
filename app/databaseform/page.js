6f0b93e0081f96cfc51a413ae03f7d723971b36a
Renu Rao <renurao.1306@gmail.com>
2023-12-25 16:04:44 +0530

    Added validation to databaseform page

--- app/databaseform/page.js
+++ app/databaseform/page.js
@@ -1,8 +1,42 @@
 'use client'
 import SidePanel from "@/app/Components/sidepanel";
 import Image from 'next/image';
+import React from "react";
 
 export default function Page() {
+    const [selectedDatabase, setSelectedDatabase] = React.useState("");
+    const [dbName, setDbName] = React.useState("");
+    const [tags, setTags] = React.useState("");
+    const [errors, setErrors] = React.useState({
+        selectedDatabase: "",
+        dbName: "",
+        tags: ""
+    });
+
+    const handleClick = () => {
+        console.log("In handle click");                             //DEBUGGING
+        const errorMessage = {selectedDatabase: "", dbName: "", tags: ""};
+
+        if(selectedDatabase === ""){
+            errorMessage.selectedDatabase = "Please select a database";
+        }
+        if(!dbName.trim()){
+            errorMessage.dbName = "Please enter a database name";
+        }
+        if(!tags.trim()){
+            errorMessage.tags = "Please enter a tag name";
+        }
+
+        setErrors((prevErrors) => ({
+            ...prevErrors,
+            ...errorMessage
+        }));
+
+        if(!Object.values(errorMessage).some((error) => error !== "")){
+            console.log(selectedDatabase, dbName, tags);            //TODO: replace with axios call
+        }
+    }
+
     return (
         <div>
             <SidePanel>
@@ -12,20 +46,25 @@ export default function Page() {
                     </h1>
                 </div>
                 <div className="flex">
-                    <form>
                         <div className="sm:col-span-4 flex-col">
                             <div className="">
                                 <label htmlFor="server" className="block text-md mt-6 font-medium leading-6 text-gray-900">
                                     Choose a database engine
+                                    <span className="text-red-500 ml-6">{errors.selectedDatabase}</span>
                                 </label>
+                                
 
                                 <div className="flex items-center mt-4 mb-4 py-4 ps-2 border border-gray-200 rounded dark:border-blue-700">
                                     <input 
                                         id="mongodb" 
                                         type="radio" 
-                                        value=""
+                                        value="mongodb"
                                         name="database" 
                                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                        onChange={(event) => {
+                                            setSelectedDatabase(event.target.value);
+                                        }}
+                                        required
                                     />
                                     <label htmlFor="mongodb" className="flex items-center ms-2 text-sm font-medium text-gray-900 dark:text-blue-800">
                                       <Image src="/assets/images/mongodb-logo-icon.png" height={40} width={40} alt="mongodb-img"/>MongoDB
@@ -35,9 +74,13 @@ export default function Page() {
                                     <input 
                                         id="postgresql" 
                                         type="radio" 
-                                        value="" 
+                                        value="postgresql"
                                         name="database" 
                                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                        onChange={(event) => {
+                                            setSelectedDatabase(event.target.value);
+                                        }}
+                                        required
                                     />
                                     <label htmlFor="postgresql" className="flex items-center ms-2 text-sm font-medium text-gray-900 dark:text-blue-800">
                                     <Image height={40} width={40} src="/assets/images/postgresql-logo-icon.png" alt="postgresql-img"/>PostgreSQL
@@ -47,9 +90,13 @@ export default function Page() {
                                     <input 
                                         id="mysql" 
                                         type="radio" 
-                                        value="" 
+                                        value="mysql"
                                         name="database" 
                                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                        onChange={(event) => {
+                                            setSelectedDatabase(event.target.value);
+                                        }}
+                                        required
                                     />
                                     <label htmlFor="mysql" className="flex items-center ms-2 text-sm font-medium text-gray-900 dark:text-blue-800">
                                         <Image height={40} width={40} src="/assets/images/mysql-logo-icon.png" alt="mysql-img"/>MySQL
@@ -59,9 +106,13 @@ export default function Page() {
                                     <input 
                                         id="redis" 
                                         type="radio" 
-                                        value="" 
+                                        value="redis"
                                         name="database" 
                                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                        onChange={(event) => {
+                                            setSelectedDatabase(event.target.value);
+                                        }}
+                                        required
                                     />
                                     <label htmlFor="redis" className="flex items-center ms-2 text-sm font-medium text-gray-900 dark:text-blue-800">
                                         <Image height={50} width={50} src="/assets/images/redis-logo-icon.png" alt="redis-img"/>Redis
@@ -81,11 +132,16 @@ export default function Page() {
                                         type="text"
                                         name="dbname"
                                         id="dbname"
+                                        value={dbName}
+                                        onChange={(event) => {
+                                            setDbName(event.target.value);
+                                        }}
                                         required
                                         className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                         placeholder="eg: db-mongodb"
                                     />
                                 </div>
+                                <span className="text-red-500">{errors.dbName}</span>
                             </div>
 
                             <div className="mb-4">
@@ -100,22 +156,27 @@ export default function Page() {
                                         type="text"
                                         name="tags"
                                         id="tags"
+                                        value={tags}
+                                        onChange={(event) => {
+                                            setTags(event.target.value);
+                                        }}
                                         required
                                         className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                         placeholder="Enter tag name"
                                     />
                                 </div>
+                                <span className="text-red-500">{errors.tags}</span>
+                            </div>
+                            <div className="gap-x-6 mr-60">
+                                <button type="submit" className="rounded-md bg-indigo-600 mt-8 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" onClick={handleClick}>
+                                    Create Database
+                                </button>
+                                <button type="button" className="text-sm font-semibold ml-10 mt-5 leading-6 text-gray-900">
+                                    Cancel
+                                </button>
                             </div>
                         </div>
-                        <div className="gap-x-6 mr-60">
-                            <button type="submit" className="rounded-md bg-indigo-600 mt-8 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                                Create Database
-                            </button>
-                            <button type="button" className="text-sm font-semibold ml-10 mt-5 leading-6 text-gray-900">
-                                Cancel
-                            </button>
-                        </div>
-                    </form>
+                        
                     <div className="ml-64">
                         <video className="w-200 h-100">
                             <source src="/assets/videos/docker_gif.webm" />
